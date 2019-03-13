78bb2040c1ddffd1a487ed42488decc903079901
HovigGH <47648973+HovigGH@users.noreply.github.com>
2019-03-12 21:45:20 -0400

    Add files via upload

---
+++ EmployeeForm1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MultiFaceRec
+{
+    public partial class EmployeeForm1 : Form
+    {
+        public EmployeeForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void upcSearchButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNameSearchButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddInvButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRefund_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUserView_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ EmployeeLogIn.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MultiFaceRec
+{
+    public partial class EmployeeLogIn : Form
+    {
+        public EmployeeLogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtPassword.Clear();
+            txtUserName.Clear();
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
